26569c34766db5f3229c4424e10aeb22330fb87b
pablo de la cruz <pablodlc@gmail.com>
2021-05-23 20:49:09 -0700

    saving per the module

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const mysql = require('mysql2');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 
 app.get('/', (req, res) => {
     res.json({
