640e8f64ff930bea61a52d31a33accab1af4bf49
pablo de la cruz <pablodlc@gmail.com>
2021-05-24 20:34:26 -0700

    getting mysql established

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO candidates (first_name, last_name, industry_connected)
 VALUES
-('Ronald', 'Firbank', 1),
+    ('Ronald', 'Firbank', 1),
     ('Virginia', 'Woolf', 1),
     ('Piers', 'Gaveston', 0),
     ('Charles', 'LeRoi', 1),
