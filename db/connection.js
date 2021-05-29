5edef70a1300b9392a777e3da399dc1e3e6faff4
pablo de la cruz <pablodlc@gmail.com>
2021-05-28 20:26:22 -0700

    housekeeping in the interest of separation of interests

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'election'
+    }
+);
+
+module.exports = db;
